11c86c4dec372b967b1d2531c453207364a05a04
rudrabarad <rudra.barad@gmail.com>
2020-04-05 17:06:57 +0530

    6

--- js/script.js
+++ js/script.js
@@ -7,27 +7,3 @@ $(document).ready(function()
     });
 });
 
-// Hover Fade In-Out On Navigation Bar Icons
-$(document).ready(function()
-{
-    $('#i-home').hover(function()
-    {
-        $('#t-home').fadeToggle();
-    });
-    $('#i-about').hover(function()
-    {
-        $('#t-about').fadeToggle();
-    });
-    $('#i-skill').hover(function()
-    {
-        $('#t-skill').fadeToggle();
-    });
-    $('#i-project').hover(function()
-    {
-        $('#t-project').fadeToggle();
-    });
-    $('#i-contact').hover(function()
-    {
-        $('#t-contact').fadeToggle();
-    });
-});
