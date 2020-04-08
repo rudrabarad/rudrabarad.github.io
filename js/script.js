e0f7792d9bfecc6ac72035399054ffc7145c6c95
rudrabarad <rudra.barad@gmail.com>
2020-04-09 00:52:44 +0530

    09/04/2020 | 01:00 AM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+// ===================================== FOR SLIDE EFFECT ==================================//
+// Click Function for Hamburger
+$(document).ready(function()
+{
+    $('.hamburger').click(function()
+    {
+        $(this).toggleClass('active');
+        $('.overlay').slideToggle(1000);
+        $('.bottom-navbar').slideToggle(1000);
+    });
+// Click in the Overlay Menu
+    $('.menu ul').click(function()
+    {
+        $('.overlay').slideToggle(100);
+        $('.bottom-navbar').slideToggle(1000);
+        $('.hamburger').toggleClass('active');        
+    });
+
+});
+
+// ===================================== FOR FADE EFFECT ==================================//
+/*
 // Click Function for Hamburger
 $(document).ready(function()
 {
@@ -6,7 +28,14 @@ $(document).ready(function()
         $(this).toggleClass('active');
         $('.overlay').fadeToggle(800);
         $('.bottom-navbar').fadeToggle(800);
-        //$('.overlay').slideToggle(1000);
-        //$('.bottom-navbar').slideToggle(1000);
     });
+// Click in the Overlay Menu
+    $('.menu ul').click(function()
+    {
+        $('.overlay').fadeToggle(400);
+        $('.bottom-navbar').fadeToggle(800);
+        $('.hamburger').toggleClass('active');        
+    });
+
 });
+*/
