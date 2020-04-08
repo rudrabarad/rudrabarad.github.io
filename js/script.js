9fa405cb4cbf6f8aab8d2118460628322e036580
rudrabarad <rudra.barad@gmail.com>
2020-04-08 23:58:04 +0530

    08/04/2020 | 12:00 Midnight

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ $(document).ready(function()
 {
     $('.hamburger').click(function()
     {
-        $(this).toggleClass('active')
+        $(this).toggleClass('active');
+        $('.overlay').fadeToggle(800);
+        $('.bottom-navbar').fadeToggle(800);
+        //$('.overlay').slideToggle(1000);
+        //$('.bottom-navbar').slideToggle(1000);
     });
 });
-
