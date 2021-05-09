413d1fb803cdc797de13c8acf2ea9eb175d3b7be
rudrabarad <rudra.barad@gmail.com>
2021-05-10 02:36:38 +0530

    Started Design Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,7 +169,7 @@
   }
 
   /**
-   * Porfolio isotope and filter
+   * Developer Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
     let portfolioContainer = select('.dev-portfolio-container');
@@ -198,38 +198,38 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
 
-  /**
-   * Initiate portfolio details lightbox 
-   */
-  const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
 
-  /**
-   * Portfolio details slider
+    /**
+   * Design Porfolio isotope and filter
    */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+     window.addEventListener('load', () => {
+      let portfolioContainer = select('.des-portfolio-container');
+      if (portfolioContainer) {
+        let portfolioIsotope = new Isotope(portfolioContainer, {
+          itemSelector: '.des-portfolio-item'
+        });
+  
+        let portfolioFilters = select('#des-portfolio-flters li', true);
+  
+        on('click', '#des-portfolio-flters li', function(e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function(el) {
+            el.classList.remove('filter-active');
+          });
+          this.classList.add('filter-active');
+  
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter')
+          });
+          portfolioIsotope.on('arrangeComplete', function() {
+            AOS.refresh()
+          });
+        }, true);
+      }
+  
+    });
+
 
   /**
    * Testimonials slider
